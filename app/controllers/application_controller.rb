644370543203c784d2129ba00a44e54375654b2c
Ziv Zamechek <ziv921@yahoo.com>
2016-05-21 10:56:51 -0400

    application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def current_cart
+    @cart = current_user.current_cart if current_user
+  end
 end
